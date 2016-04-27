e429453e4a70fa40d698c923710f678146ad420d
Skrelan <skrelan@gmail.com>
2016-04-26 17:18:10 -0700

    finding subsets

--- Recursion/simple.py
+++ Recursion/simple.py
@@ -1,18 +1,20 @@
+#SUBSET PROB
+
 # /********************************************************************** 
-#  *                          			                                *
-#  *                                                                    *
-#  *  Prompt: Given a set S, return the power set P(S), which is        *
-#  *          a set of all subsets of S.                                *
-#  *                                                                    *
-#  *  Input:  A String                                                  *
-#  *  Output: An Array with the power set of the input string           *
-#  *                                                                    *
-#  *  Example: S = "abc", P(S) = ['', 'a', 'b','c','ab','ac','bc','abc']*                                                               *
-#  *                                                                    *
-#  *  Note: There should not be any duplicates in the power set         *
-#  *        ('ab' and 'ba' are considered the same), and it will always *
-#  *        begin with an empty string ('')                             *
-#  *                                                                    *
+#  *                          			                                																						*
+#  *                                                                    																							*
+#  *  Prompt: Given a set S, return the power set P(S), which is        													*
+#  *          a set of all subsets of S.                                																			*
+#  *                                                                    																							*
+#  *  Input:  A String                                                  																				*
+#  *  Output: An Array with the power set of the input string           														*
+#  *                                                                    																							*
+#  *  Example: S = "abc", P(S) = ['', 'a', 'b','c','ab','ac','bc','abc']                                                         *
+#  *                                                                    																							*
+#  *  Note: There should not be any duplicates in the power set         													*
+#  *        ('ab' and 'ba' are considered the same), and it will always 														*
+#  *        begin with an empty string ('')                             																		*
+#  *                                                                    																							*
 #  **********************************************************************/
 
 def powerSet(input):  #i/p - abcd o/p - "" a b c d ab ac ad bc bd cd 
