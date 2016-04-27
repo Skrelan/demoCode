4368a20673300a67083026bf0f0baade283de81e
Skrelan <skrelan@gmail.com>
2016-04-26 17:13:55 -0700

    Merge Sort in Python

---
+++ Recursion/Sorts/merge-sort.py
@@ -0,0 +1,37 @@
+def  MergeSort(array):
+		Result = array
+		if len(array)>1:
+			mid = len(array)//2
+			left = array[:mid]
+			right = array[mid:]
+			MergeSort(left)
+			print "Left is done",left
+			MergeSort(right)
+			print "Right is done",right
+			i = 0
+			j = 0
+			k = 0
+			while (i < len(left) )& (j <len(right)):
+				if left[i] < right[j]:
+					array[k] = left[i]
+					i = i + 1
+				else:
+					array[k] = right[j]
+					j = j + 1
+				k = k + 1
+			while i < len(left):
+					array[k]=left[i]
+					i = i + 1
+					k = k + 1
+			while j < len(right):
+					array[k]=right[j]
+					j = j+ 1
+					k = k + 1
+		
+			
+		#print Result
+
+arr = map(int,(raw_input("Enter Array of numbers seperated by single spaces: ").split(" ")))
+print "Before Sort",arr
+MergeSort(arr)
+print "After Sort",arr		
--- Sorts/merge-sort.py
+++
@@ -1,7 +0,0 @@
-def  MergeSort(arr):
-		stack = []
-		def Split(array):
-			
-			left = [:(len(array)/2)]
-			right = [(len(array)/2):]
-			
